479083148283329c8725a680707549130394bd55
Jason Pursey <jason@unumotors.com>
2019-09-14 22:48:50 +0200

    Add index.html

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const fs = require('fs')
 const { Server } = require('./lib/server')
 const { Response } = require('./lib/server/response')
 
 const server = new Server()
 
 server.get('/ping', () =>  new Response(200, 'OK'))
+server.get('/', () => new Response(200, fs.readFileSync('./index.html'), 'html'))
 server.listen()
